96ca35cb492bc17558ed4da2992b40b5ff260d6f
Navid Rojiani <nrojiani@gmail.com>
2018-02-16 21:29:10 -0800

    runnable app

---
+++ src/main/kotlin/tutorial/Application.kt
@@ -0,0 +1,23 @@
+package tutorial
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Configuration
+
+
+/**
+ * The main entry point to the application
+ *
+ * [Tutorial](https://scotch.io/@grahamcox82/how-to-build-a-simple-rest-api-with-kotlin-and-spring-boot)
+ */
+@EnableAutoConfiguration
+@Configuration
+class Application
+
+/**
+ * Run the application
+ * @param args The command line arguments
+ */
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
+}
